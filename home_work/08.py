12fe2d4420cfc56ee73bfc4c57313c36c2bf8141
IhorRiznichenko <125815727+IhorRiznichenko@users.noreply.github.com>
2023-03-09 17:12:34 +0200

    home work lesson_06

---
+++ home_work/08.py
@@ -0,0 +1,28 @@
+# Сделать программу в виде функций в которой нужно будет угадывать число.
+def game():
+    global number
+    import random
+    num = random.randint(1, 5)
+    attempts = 0
+    print('Отгадайте число между 1 и 99.')
+    while attempts < 5:
+        number = int(input('Введите число: '))
+        attempts += 1
+        if number < num:
+            print('Ваше число меньше загаданного.')
+        if number > num:
+            print('Ваше число больше загаданного.')
+        if number == num:
+            break
+    if number == num:
+        print('Вы угадал число {0}, количество попыток {1}'.format(num, attempts))
+    else:
+        print('Вы не угадал число! Загаданное число {0}'.format(num))
+game()
+while True:
+    question = input("Желаете повторить? (Д/Н)\n")
+    if question.upper() in ('Д', 'Y'):
+        game()
+    elif question.upper() in ('Н', 'N'):
+        print('Спасибо, до встречи')
+        break
