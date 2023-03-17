455de4b394f4ad1b4cc0c1d9112a0b6765365eff
IhorRiznichenko <125815727+IhorRiznichenko@users.noreply.github.com>
2023-03-17 11:23:50 +0200

    home work lesson_09

---
+++ home_work/13_2.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+
+def time_it(func_1):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        result = func_1(*args, **kwargs)
+        end = datetime.now()
+        elapsed = end - start
+        print(f'Время выполнения функции: {elapsed}')
+        return result
+    return wrapper
+
+
+@time_it
+def func(word):
+    if word == 'Yes':
+        return True
+    else:
+        return False
+
+
+@time_it
+def area(b, h):
+    return 0.5 * b * h
+
+
+func('Yes')
+area(2, 5)
