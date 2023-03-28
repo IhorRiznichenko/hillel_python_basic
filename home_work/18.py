d505b2e4e5f1a249247482c7277ae612d1a700b2
IhorRiznichenko <125815727+IhorRiznichenko@users.noreply.github.com>
2023-03-28 15:36:28 +0300

    home work lesson_11

---
+++ home_work/18.py
@@ -0,0 +1,25 @@
+import csv
+import json
+import random
+
+with open('my_dict.json', 'r') as file:
+    my_dict = json.load(file)
+
+operators = ['095', '066', '098', '096', '050', '097']
+
+for key in my_dict:
+    if random.random() < 0.75:
+        operator = random.choice(operators)
+        phone_number = operator + ''.join(random.choice('0123456789') for _ in range(7))
+    else:
+        phone_number = ''
+    my_dict[key] = list(my_dict[key])
+    my_dict[key].extend([phone_number])
+    my_dict[key] = tuple(my_dict[key])
+
+with open('my_dict.csv', 'w', newline='') as write_file:
+    writer = csv.writer(write_file)
+    writer.writerow(['ID', 'Имя', 'Возраст', 'Телефон'])
+    for key in my_dict:
+        row = [key] + list(my_dict[key])
+        writer.writerow(row)
