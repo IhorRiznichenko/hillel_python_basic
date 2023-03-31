9481a98338358837492a99563e7316ec3b4089b7
IhorRiznichenko <125815727+IhorRiznichenko@users.noreply.github.com>
2023-03-31 16:07:40 +0300

    home work lesson_12

---
+++ home_work/20.py
@@ -0,0 +1,16 @@
+class Auto:
+    def __init__(self, brand, age, color, mark, weight):
+        self.brand = brand
+        self.age = age
+        self.color = color
+        self.mark = mark
+        self.weight = weight
+
+    def move(self):
+        print("move")
+
+    def birthday(self):
+        self.age += 1
+
+    def stop(self):
+        print("stop")
