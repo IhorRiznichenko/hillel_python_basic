2fbaf41a93a5c19a2640941e646050d76a3e7c7e
IhorRiznichenko <125815727+IhorRiznichenko@users.noreply.github.com>
2023-04-05 10:22:29 +0300

    home work lesson_13

---
+++ home_work/22.py
@@ -0,0 +1,66 @@
+import math
+
+
+class Point():
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x, y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def distance_from_origin(self):
+        return math.hypot(self.x, self.y)
+
+
+class Circle(Point):
+
+    def __init__(self, radius, x=0, y=0):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __str__(self):
+        return super().__str__()[:-1] + f', radius={self.radius})'
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        radius = self.radius + other.radius
+        return Circle(radius, x, y)
+
+    def edge_distance_from_origin(self):
+        return abs(self.distance_from_origin() - self.radius)
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
+
+    def area(self):
+        return math.pi * (self.radius**2)
+
+
+    def __sub__(self, other):
+        if self.radius == other.radius:
+            return Point(self.x - other.x, self.y - other.y)
+        else:
+            radius = abs(self.radius - other.radius)
+            return Circle(radius, self.x - other.x, self.y - other.y)
+
+
+circle1 = Circle(3, 1, 1)
+circle2 = Circle(2, 2, 2)
+circle3 = Circle(3, 1, 1)
+
+print(circle1 - circle2)
+print(circle1 - circle3)
