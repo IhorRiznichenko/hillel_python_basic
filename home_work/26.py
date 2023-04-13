7e68ff1d038d85f8b522cc7f03ef9e47e5e20fe3
IhorRiznichenko <125815727+IhorRiznichenko@users.noreply.github.com>
2023-04-13 09:34:43 +0300

    home work lesson_16

---
+++ home_work/26.py
@@ -0,0 +1,16 @@
+def simple_generator(start, step):
+    yield start
+    while True:
+        start *= step
+        yield start
+
+
+for item in simple_generator(-2, -5):
+    print(item)
+    if -1000 <= item >= 1000:
+        break
+
+for item in simple_generator(10, 3):
+    print(item)
+    if -1000 <= item >= 1000:
+        break
